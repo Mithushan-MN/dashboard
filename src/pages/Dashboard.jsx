43a86411e44e56587f11977404ce075c4e0d5178
Mithushan-MN <mithushan0099@gmail.com>
2023-06-17 09:30:10 +0530

    commit table

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -53,45 +53,128 @@
 
 
 
-import React, { useEffect, useState } from 'react'
+//import React, { useEffect, useState } from 'react'
 import '../styles/Dashboard.css'
-import axios from 'axios'
+
+//import axios from 'axios'
 
 
 
-function Dashboard(){
-  const [data,setData] = useState([])
-  useEffect(()=>{
-    axios.get("http://localhost:3000/users")
-    .then(res =>setData(res.data))
-    .catch(er =>console.log(er));
-  },[])
+// function Dashboard(){
+//   const [data,setData] = useState([])
+//   useEffect(()=>{
+//     axios.get("http://localhost:3000/users")
+//     .then(res =>setData(res.data))
+//     .catch(er =>console.log(er));
+//   },[])
+//   return (
+//    <div className='dashboard'>
+//     <table>
+//       <tread>
+//         <tr>
+          
+//           <th>DISPLAY NAME</th>
+//           <th>EMAIL</th>
+//           <th>USER TYPE</th>
+//           <th>USER STATUS</th>
+//         </tr>
+//       </tread>
+//       <tbody>
+//         {
+//           data.map((user,index) =>(
+//             <tr key={index}>
+//               <td>{user.NAME}</td>
+//               <td>{user.EMAIL}</td>
+//               <td>{user.USERTYPE}</td>
+//               <td>{user.USERSTATUS}</td>
+//             </tr>
+//           ))
+//         }
+//       </tbody>
+//     </table>
+//    </div>
+//   )
+// }
+
+// export default Dashboard
+
+// App.js
+import { useEffect, useState } from "react";
+//import "./App.css";
+
+function Dashboard() {
+  const [data, setData] = useState([]);
+  
+
+  const fetchData = () => {
+    
+    fetch('db.json',{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+      .then((response) => response.json())
+      .then((actualData) => {
+        console.log(actualData);
+        setData(actualData.users);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-   <div className='dashboard'>
-    <table>
-      <tread>
+    <div className="App">
+      <div className="filters">
+        <div className='filter_left'>
+          <h5 className='active__'>Active Users<i class="ri-arrow-down-s-line"></i></h5>
+          <h5 className='deactive'><i class="ri-search-line"></i>Search Users</h5>
+          <h5 className='active_middle'><i class="ri-filter-3-line"></i>Add filter</h5>
+        </div>
+
+        <div className="filter_right">
+          <h5><i class="ri-add-line"></i>Add Users<i class="ri-arrow-down-s-line"></i></h5>
+          <h5 className='deactive'><i class="ri-delete-bin-6-line"></i>Delete</h5>
+          <h5 className='deactive'><i class="ri-key-2-line"></i>Reset password</h5>
+        </div>
+        
+      </div>
+
+      <div className='filter__button'>
+        <button>user type: Member <i class="ri-close-line"></i></button>
+        <button>status: active <i class="ri-close-line"></i></button>
+        <h5>Clear Filter</h5>
+      </div>
+
+
+      <tbody>
         <tr>
           <th>DISPLAY NAME</th>
           <th>EMAIL</th>
           <th>USER TYPE</th>
           <th>USER STATUS</th>
+          <th></th>
+         
         </tr>
-      </tread>
-      <tbody>
-        {
-          data.map((user,index) =>(
-            <tr key={index}>
-              <td>{user.NAME}</td>
-              <td>{user.EMAIL}</td>
-              <td>{user.USERTYPE}</td>
-              <td>{user.USERSTATUS}</td>
-            </tr>
-          ))
-        }
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td>{item.NAME}</td>
+            <td>{item.EMAIL}</td>
+            
+            <td>{item.USERTYPE}</td>
+            <td>{item.USERSTATUS}</td>
+            <td>{item.USERST}</td>
+          </tr>
+        ))}
       </tbody>
-    </table>
-   </div>
-  )
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
